d069365f095dd82a05c34460bc85b3f4544faa8e
alvo254 <alvinkhamron@gmail.com>
2023-07-05 22:35:08 +0300

    cidr

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -11,7 +11,7 @@ variable "project" {
 variable "environment" {
     type = string
     description = "Environment"
-    default = "toast"
+    default = "Dev"
 }
 
 
@@ -20,29 +20,29 @@ variable "region" {
 }
 
 variable "public_subnet" {
-  default = "10.10.0.0/24"
+  default = "10.0.0.0/24"
 }
 
 variable "public_subet_2" {
-  default = "10.0.0.0/24"
+  default = "10.0.10.0/24"
 }
 
 
 variable "private_subnet"{
-  default = "10.10.0.0/24"
+  default = "10.0.20.0/24"
     
 }
 
 variable "private_subnet_2"{
-   default = "10.1.0.0/24"
+   default = "10.0.30.0/24"
     
 }
 
 variable "private_data"{
-  default = "10.5.0.0/24"
+  default = "10.0.40.0/24"
 
 }
 
 variable "private_data_2"{
-     default = "10.15.0.0/24"
+     default = "10.0.50.0/24"
 }
