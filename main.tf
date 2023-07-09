ccabceac6d1211e8076b16a35f85dfa5b0343c1d
Blessing U <info@cecureintel.com>
2023-07-10 00:08:30 +0100

    TF fmt on dev

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 module "vpc" {
   source = "./modules/vpc"
-  
+
 }
 
 //public instance 
 module "ec2" {
-	source = "./modules/ec2"
+  source = "./modules/ec2"
   vpc_id = module.vpc.vpc_id
   subnet = module.vpc.public_subnet_az1_id
 }
@@ -13,5 +13,5 @@ module "ec2" {
 # module "sg" {
 #   source = "./modules/sg"
 #   vpc_id = module.vpc.vpc_id
-  
+
 # }
