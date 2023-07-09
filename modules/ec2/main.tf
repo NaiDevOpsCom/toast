e01ab098b8a4c0ea6ab04e286abf0f1f98ad8a56
alvo254 <alvinkhamron@gmail.com>
2023-07-09 21:51:26 +0300

    update

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -6,15 +6,15 @@ resource "aws_instance" "alvo-toast" {
   //This is interpolation or directive
   key_name = "${aws_key_pair.deployer.key_name}"
 
-  user_data = data.template_file.user_data.rendered
+  # user_data = data.template_file.user_data.rendered
 
-  # 	user_data = <<-EOF
-	# 	#!/bin/bash
-  #   sudo apt-get update
-	# 	sudo apt install nginx
-	# 	sudo systemctl start nginx
-	# 	sudo systemctl enable nginx
-	# EOF
+  	user_data = <<EOF
+		#! /bin/bash
+    sudo apt-get update
+		sudo apt install nginx
+		sudo systemctl start nginx
+		sudo systemctl enable nginx
+	EOF
 
   vpc_security_group_ids = [aws_security_group.alvo-toast.id]
   # vpc_security_group_ids = var.security_gp_id
@@ -47,9 +47,9 @@ resource "local_file" "alvo-ssh-keys" {
 }
 
 
-data "template_file" "user_data" {
-  template = "install_nginx.sh"
-}
+# data "template_file" "user_data" {
+#   template = "${file("install_nginx.sh")}"
+# }
 
 
 resource "aws_security_group" "alvo-toast" {
